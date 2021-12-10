98aefc7c73898677a20d46fafe794f2eba1080c5
Michel Romero <studentenherz@gmail.com>
2021-12-10 03:01:16 -0300

    added something

--- main.py
+++ main.py
@@ -78,4 +78,9 @@ class SetScore(Resource):
 api.add_resource(SetScore, '/setScore')
 api.add_resource(GetScoreBoard, '/getScoreBoard')
 
-app.run(host = '0.0.0.0', port= int( os.environ.get('PORT', 33507) ))
+@app.route("/")
+def hello():
+    return "<h1 style='color:blue'>Hello There!</h1>"
+
+if __name__ == '__main__':
+	app.run(host = '0.0.0.0')
